5074580db06613e1b4bda55ffe4904f80188443e
machukhinktato <tarabrinmv@gmail.com>
2021-01-23 15:21:08 +0300

    in progress

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello World! You at the polls index!")
+    lateest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in lateest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+def results(request, question_id):
+    response = f"You're looking at answer {question_id}"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on {question_id}")
