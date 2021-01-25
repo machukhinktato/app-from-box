b2a8953412a5ed7a295acd57bf60a9c4f50baf4b
machukhinktato <tarabrinmv@gmail.com>
2021-01-25 18:28:52 +0300

    in progress

--- appfrombox/urls.py
+++ appfrombox/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
+from . import views
+
+app_name = 'appfrombox'
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
