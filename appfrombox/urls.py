0f1929b15bc0848d35ac41d73b3541e3f025ea65
machukhinktato <tarabrinmv@gmail.com>
2021-01-26 18:47:15 +0300

    in progress

--- appfrombox/urls.py
+++ appfrombox/urls.py
@@ -19,10 +19,8 @@ from django.urls import include, path
 
 from . import views
 
-app_name = 'appfrombox'
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
