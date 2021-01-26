537d80a860bdc1eadc4b2600b860e994b9fea507
machukhinktato <tarabrinmv@gmail.com>
2021-01-26 19:10:44 +0300

    in progress

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 app_name = 'polls'
 
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
 
 """ its could looks like this """
 # urlpatterns = [
@@ -11,10 +17,3 @@ app_name = 'polls'
 #     path('<int:question_id>/results/', views.results, name='results'),
 #     path('<int:question_id>/vote/', views.vote, name='vote'),
 # ]
-
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-]
